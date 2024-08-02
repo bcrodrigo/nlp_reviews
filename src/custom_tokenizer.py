9232333e1e859dcd06f6555b26148f1bc5174eac
RodrigoBC <bcrodrigo16@gmail.com>
2024-08-01 19:08:42 -0700

    adding custom_tokenizer.py to src folder

---
+++ src/custom_tokenizer.py
@@ -0,0 +1,37 @@
+import spacy
+
+
+def tokenizer_and_lemma(sentence):
+    """
+    Function to perform tokenization and lemmatization using the spaCy library.
+    The tokenizer removes the following from an input sentence:
+        - punctuation
+        - stop words
+        - blank spaces
+        - digits
+        - currency symbols
+
+    Parameters
+    ----------
+    sentence : str
+        A single string containing a sentence to be tokenized
+
+    Returns
+    -------
+    List
+        A list of lemmatized tokens
+    """
+    nlp = spacy.load("en_core_web_sm")
+    document = nlp(sentence)
+
+    token_list = [
+        token.lemma_
+        for token in document
+        if not token.is_punct
+        and not token.is_stop
+        and not token.is_space
+        and not token.is_digit
+        and not token.is_currency
+    ]
+
+    return token_list
